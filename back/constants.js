540f31ba4cf54ae21bd456a9fdd2590c683606b5
rakbir <brianrakotoarisoa@gmail.com>
2022-04-11 23:25:49 +0300

    mongo atlas url

--- back/constants.js
+++ back/constants.js
@@ -1,5 +1,5 @@
 module.exports={
-	url:"mongodb://localhost:27017",
+	url:"mongodb+srv://ekaly:H6jMiXRUvSkU9tV7@cluster0.a6ng3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	db:"meandb",
 	secret:"kaizakaiza"
 }
