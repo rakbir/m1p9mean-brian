7a07e378ebce040d9375df527b9193e266c67443
Brian <brianrakotoarisoa@gmail.com>
2022-04-12 09:33:37 +0300

    update environment.prod.ts

--- angularApp/src/environments/environment.prod.ts
+++ angularApp/src/environments/environment.prod.ts
@@ -1,3 +1,25 @@
 export const environment = {
   production: true
 };
+
+
+export const urls={
+  login:"https://m1p9mean-brian-back.herokuapp.com/utilisateurs/login",
+  mail_login:"https://m1p9mean-brian-back.herokuapp.com/utilisateurs/mail-login",
+  user_session:"https://m1p9mean-brian-back.herokuapp.com/utilisateurs/session-user",
+  liste_restaurants:"https://m1p9mean-brian-back.herokuapp.com/restaurants/liste",
+  info_restaurant:"https://m1p9mean-brian-back.herokuapp.com/restaurants/info",
+  carte_restaurant:"https://m1p9mean-brian-back.herokuapp.com/plats/carte",
+  nouvelle_commande:'https://m1p9mean-brian-back.herokuapp.com/commandes/nouveau',
+  inscription:'https://m1p9mean-brian-back.herokuapp.com/utilisateurs/inscription',
+  en_cours:'https://m1p9mean-brian-back.herokuapp.com/commandes/en-cours',
+  pret_pour_livraison:"https://m1p9mean-brian-back.herokuapp.com/commandes/prete-pour-livraison",
+  a_livrer:"https://m1p9mean-brian-back.herokuapp.com/commandes/a-livrer",
+  livraison:'https://m1p9mean-brian-back.herokuapp.com/commandes/livraison',
+  liste_plats:"https://m1p9mean-brian-back.herokuapp.com/plats/liste",
+  nouveau_plat:"https://m1p9mean-brian-back.herokuapp.com/plats/nouveau",
+  supprimer_plat:'https://m1p9mean-brian-back.herokuapp.com/plats/supprimer',
+  liste_commandes:"https://m1p9mean-brian-back.herokuapp.com/commandes/voir",
+  liste_livreurs:'https://m1p9mean-brian-back.herokuapp.com/livreurs/liste',
+  deconnexion:"https://m1p9mean-brian-back.herokuapp.com/utilisateurs/deconnexion"
+}
